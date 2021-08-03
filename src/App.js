35f0ed437d2df06f51fe8e943d944d8322405576
rrhodes <rrhodes@atb.com>
2021-08-03 14:26:20 -0600

    setup budget screen and style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import {
+  Container,
+  Header,
+  Statistic,
+  Segment,
+  Form,
+  Grid,
+  Icon,
+  Button,
+} from 'semantic-ui-react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Header as="h1">Budget</Header>
+
+      <Statistic size="small">
+        <Statistic.Label>Your Balance:</Statistic.Label>
+        <Statistic.Value>2,550.53</Statistic.Value>
+      </Statistic>
+
+      <Segment textAlign="center">
+        <Grid columns={2}>
+          <Grid.Row>
+            <Grid.Column>
+              <Statistic size="tiny" color="green">
+                <Statistic.Label>Income:</Statistic.Label>
+                <Statistic.Value>1,045.50</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Statistic size="tiny" color="red">
+                <Statistic.Label>Expenses:</Statistic.Label>
+                <Statistic.Value>623.50</Statistic.Value>
+              </Statistic>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Header as="h3">History</Header>
+
+      <Segment color="red">
+        <Grid columns={3}>
+          <Grid.Row>
+            <Grid.Column width={10}>Something</Grid.Column>
+            <Grid.Column width={3}>$10.00</Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment color="green">
+        <Grid columns={3}>
+          <Grid.Row>
+            <Grid.Column width={10}>Something Else</Grid.Column>
+            <Grid.Column width={3}>$100.00</Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment color="red">
+        <Grid columns={3}>
+          <Grid.Row>
+            <Grid.Column width={10}>Another thing</Grid.Column>
+            <Grid.Column width={3}>$50.00</Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name="edit" bordered />
+              <Icon name="trash" />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      <Header as="h3">Add New Transaction</Header>
+      <Form unstackable>
+        <Form.Group>
+          <Form.Input
+            icon="tags"
+            width={12}
+            label="Description"
+            placeholder="New Shiny Thing"
+          />
+          <Form.Input
+            width={4}
+            label="Value"
+            placeholder="100.00"
+            icon="dollar"
+            iconPosition="left"
+          />
+        </Form.Group>
+        <Button.Group style={{ marginTop: 20 }}>
+          <Button>Cancel</Button>
+          <Button.Or />
+          <Button primary>Ok</Button>
+        </Button.Group>
+      </Form>
+    </Container>
+  )
 }
 
-export default App;
+export default App
